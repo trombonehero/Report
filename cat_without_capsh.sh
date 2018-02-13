9568859a7836d07c492264e52c006165f32fb6b2
Stanley Uche Godfrey <ujay2002@yahoo.com>
2018-02-13 10:38:30 -0330

    Add files via upload

---
+++ cat_without_capsh.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+	
+	time  cat ./tenMeg.txt>output.txt
+	time   cat ./fiftyMeg.txt>output.txt
+	time   cat ./hMeg.txt>output.txt
+	time  cat ./twoHMeg.txt>output.txt
+	time  cat ./twoHMegPlus.txt>output.txt
+	time  cat ./fiveHMeg.txt>output.txt
+	time  cat ./SevenMeg.txt>output.txt
+	time  cat ./oneGig.txt>output.txt
+
